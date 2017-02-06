5b830cb957c1aef961ad1f8dc07b8f8fc78caaa1
FriedrichXing <214755103@qq.com>
2017-02-06 15:14:16 +0800

    GoodsMgr,OrderMgr

---
+++ js/addgoodsbrand.data.js
@@ -0,0 +1,13 @@
+var goodstypeData = [{
+	value: '0',
+	text: '一级品牌分类',
+},{
+	value: '1',
+	text: '金翌',
+},{
+	value: '2',
+	text: '鑫洋',
+}, {
+	value: '3',
+	text: '其他',
+}]
---
+++ js/addgoodstype.data.js
@@ -0,0 +1,66 @@
+var goodstypeData = [{
+	value: '0',
+	text: '一级商品分类',
+//	children: [{
+//		value: "0",
+//		text: "一级商品分类"
+//	}]
+},{
+	value: '1',
+	text: '电动车',
+	children: [{
+		value: "0",
+		text: "二轮电动车"
+	}, {
+		value: "1",
+		text: "三轮电动车"
+	}, {
+		value: "2",
+		text: "四轮电动车"
+	}]
+}, {
+	value: '2',
+	text: '摩托车',
+	children: [{
+		value: "0",
+		text: "二轮摩托车"
+	}, {
+		value: "1",
+		text: "三轮摩托车"
+	}, {
+		value: "2",
+		text: "四轮摩托车"
+	}]
+}, {
+	value: '3',
+	text: '新能源汽车',
+	children: [{
+		value: "0",
+		text: "能源汽车"
+	}, {
+		value: "1",
+		text: "电动能源车"
+	}]
+},  {
+	value: '4',
+	text: '配件',
+	children: [{
+		value: "0",
+		text: "发动机"
+	},{
+		value: "1",
+		text: "电池"
+	},{
+		value: "2",
+		text: "轮胎"
+	},{
+		value: "3",
+		text: "化油器"
+	},{
+		value: "4",
+		text: "倒挡器"
+	},{
+		value: "5",
+		text: "仪表"
+	},]
+}]
